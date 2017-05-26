025f1b6e69e7b43a404b302d758595decc6ca65b
Steff McKee <steffmckee@outlook.com>
2017-05-26 09:30:31 -0600

    I love checking in code

---
+++ CustomEnumerator/CustomEnumerator/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    public class Car
+    {
+        public const int MaxSpeed = 100;
+
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        private bool carIsDead;
+
+        private Radio theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Console.WriteLine("{0} is out of order...", PetName);
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                } else
+                {
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
--- CustomEnumerator/CustomEnumerator/Program.cs
+++ CustomEnumerator/CustomEnumerator/Program.cs
@@ -10,6 +10,16 @@ namespace CustomEnumerator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with IEnumerable / IEnumerator *****\n");
+            Garage carLot = new Garage();
+
+            // Hand over each car in the collection?
+            foreach (Car c in carLot)
+            {
+                Console.WriteLine("{0} is going {1} MPH.", c.PetName, c.CurrentSpeed);
+            }
+
+            Console.ReadLine();
         }
     }
 }
